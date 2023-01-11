a19775bf81af35c713450282c44fb3b31a23f1f9
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2023-01-11 17:49:44 +0800

    Create vedioPrecess.py

---
+++ 04-源码/vedioPrecess.py
@@ -0,0 +1,67 @@
+#coding=utf8
+import sys, os
+import requests, json
+import urllib.request
+
+def downLoadTs(siteFile, downloadPath, namePrefix):
+    with open(siteFile, "r") as f:        
+        count = 0 
+        for line in f:
+            if line.startswith("#"):
+               continue
+
+            line = line.strip()
+            url = line 
+            count = count + 1 
+            tsDownload = None
+            try:
+                tsDownload = urllib.request.urlopen(url)
+                data = tsDownload.read()
+                with open(downloadPath + "/" + namePrefix + "-"+ count + ".ts", "wb") as tsFile:
+                    tsFile.write(data)
+            except:
+                print("down load error, url:", url, "status:", tsDownload)
+                return False
+        return True
+
+      
+def ts2MP4(tsPath, mp4Path):
+   if tsPath == '' or  mp4path == '':
+      return False  
+
+   with open(mp4Path + ".mp4", "wb") as mp4f:
+       with open(tsPath, "r") as tsf:
+            mp4f.write(tsf)
+   
+   return True
+
+def tsProcess(siteFile, downloadPath, namePrefix):
+    newPath = downloadPath + "/" + namePrefix
+    if not os.path.isdir(newPath):
+       os.makedirs(newPath)
+
+    ret = downLoadTs(siteFile, newPath, namePrefix)
+    
+    if not ret:
+       return
+
+    fs = os.listdir(downloadPath)
+    for f in fs:
+        tmp = os.path.join(downloadPath, f)
+        if os.path.isfile(tmp):
+           ret2 = ts2MP4(tmp, downloadPath + namePrefix)
+           if not ret2:
+              print("ts2MP4 fail, filename:", tmp)
+              continue
+
+        elif os.path.isdir(tmp):
+           func(tmp) # 递归去获取
+        else:
+           print('其他情况')
+
+  
+if __name__ == '__main__':
+   
+   path = "/Users/chaoxi.ycx/ycx_work/sourecode/maotai"
+   siteFile = "/Users/chaoxi.ycx/ycx_work/sourecode/maotai/视频链接地址.m3u8"   
+   tsProcess(siteFile, path, "视频")
