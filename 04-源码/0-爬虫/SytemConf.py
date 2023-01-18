dc25de6ea8a53963d44160caa4d92a51431e0bd5
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2023-01-18 16:04:38 +0800

    系统配置项

--- 04-源码/0-爬虫/SytemConf.py
+++ 04-源码/0-爬虫/SytemConf.py
@@ -4,6 +4,12 @@ projectZip = 'https://github.com/chaoxiyan1225/vedioprocess/archive/refs/heads/m
 userConf = 'users.conf'
 softwareConf = 'software.conf'
 contackQQ = '59478148'
-email = '59478148@qq.com'
+email = 'chaoxiyan1225@163.com'
 newSoftWareZip = ''
 clientVersion = '1.0.6' # 客户端的版本号，随着时间递增，这个值会不断变化，不会超过服务端的值
+emailFrom = email
+emailPwd = 'VxqRl8Zr1ekcTvzxSndoUw=='
+emailSender = 'SPEEEEEEED'
+contackUs = f'QQ:{contackQQ} , email:{email}'
+
+
