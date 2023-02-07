262ab704f2706ce33ead63966b198d5a3f67c8db
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2023-02-07 17:22:29 +0800

    Update Errors.py

--- 04-源码/0-爬虫/Errors.py
+++ 04-源码/0-爬虫/Errors.py
@@ -5,7 +5,6 @@ class BaseError:
     	self.code = code 
     	self.message = message
 
-
     def getCode(self):
     	return self.code
 
@@ -14,6 +13,7 @@ class BaseError:
 
     def append(self, moreMsg):
     	self.message = f'{self.message},and {moreMsg}'
+
     	return self
 
     def toString(self):
@@ -31,13 +31,11 @@ FAIL    = BaseError("111111", "FAIL")
 '''
   客户端的错误码集合，都是以0c_ 开头 
 '''
-C_LoginFail       = BaseError("0c_0001", "客户端登陆失败！")
+C_LoginFail         = BaseError("0c_0001", "客户端登陆失败！")
 C_ErrorPasswdOrName = BaseError("0c_0002", "用户名或者密码错误")
-C_Arrearage       = BaseError("0c_0003", "客户欠费")
-C_InvalidUser     = BaseError("0c_0004", "无效的用户ID")
-C_VersionTooLow   = BaseError("0c_0005", "客户端软件版本太低")
-C_EmailWrong      = BaseError("0c_0006", "email 信息错误")
-C_SendEmailFail   = BaseError("0c_0007", "发送邮件错误")
+C_Arrearage         = BaseError("0c_0004", "无效的用户ID")
+C_VersionTooLow     = BaseError("0c_0006", "email 信息错误")
+C_SendEmailFail     = BaseError("0c_0007", "发送邮件错误")
 
 #----------------------------------------------------------
 '''
@@ -48,3 +46,4 @@ S_DownLoadError      = BaseError("0s_0001", "服务端下载失败")
 S_InvalidFileContent = BaseError("0s_0002", "下载的文件内容为空")
 S_ParseFail          = BaseError("0s_0003", "服务端文件解析异常")
 S_Forbidden          = BaseError("0s_0004", "该软件当前禁用!!")
+S_NoAdviceUrl        = BaseError("0s_0005", "无建议的网址")
