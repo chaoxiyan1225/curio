330ff2ea66c83542c9f5f429071896f3bee444ee
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2023-08-16 20:27:45 +0800

    Update main_tkinter.py

--- 04-sourcecode/0-parser/vediodownloader/main_tkinter.py
+++ 04-sourcecode/0-parser/vediodownloader/main_tkinter.py
@@ -19,7 +19,9 @@ from conf.pictures_v3 import *
 
 PIC_SIZE = 30
 FRAME_NAMES = ["", "", ""]
-PROGRESS_INFO = "【下载详情】共UU个链接,正在下载第:CC个链接,本链接共有:TT个视频,下载成功:SS个,失败:FF,是否下载完成:YY"
+PROGRESS_INFO = "【下载详情】共UU个链接,正在下载第:CC个链接,本链接共有:TT个视频,已下载成功:SS个,失败:FF,是否下载完成:YY  "
+
+STATUS_INFO = "【NOTES】:您当前处于:cc, ss!"
 
 class App(customtkinter.CTk):
 
@@ -73,8 +75,6 @@ class App(customtkinter.CTk):
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
         self.download_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="DownLoad", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.download_img, anchor="w", command=self.download_button_event)
         self.download_button.grid(row=1, column=0, sticky="ew")
-        self.shares_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Shares", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.shares_img, anchor="w", command=self.shares_button_event)
-        self.shares_button.grid(row=2, column=0, sticky="ew")
         self.register_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Register", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.register_img, anchor="w", command=self.register_button_event)
         self.register_button.grid(row=3, column=0, sticky="ew")
         self.aboutus_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="AboutUs", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.aboutus_img, anchor="w", command=self.aboutus_button_event)
@@ -89,41 +89,40 @@ class App(customtkinter.CTk):
         self.download_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         #self.download_frame.grid(row=0, column=1,  sticky="nsew")
         self.download_frame.grid_columnconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15), weight=1)
-        self.download_frame.grid_rowconfigure((0,1,2,3,4,5,6,7,8,9), weight=1)
+        self.download_frame.grid_rowconfigure((0,1,3,4,5,6,7,8,9), weight=1)
 
-        self.info_label =  customtkinter.CTkLabel(self.download_frame,corner_radius=0, height=15,  text="请粘贴视频网址:",
+        self.info_label =  customtkinter.CTkLabel(self.download_frame,corner_radius=0, height=15,  text="视频网址:",
                                                        fg_color="transparent", text_color=("gray10", "gray90"),
                                                       anchor="w")
         self.info_label.grid(row=1, column=0, padx=(20,0), pady=(0,0))
-        
-        
-        self.add_button =  customtkinter.CTkButton(self.download_frame, corner_radius=0, width=15,text="", fg_color="transparent",hover_color=("gray70", "gray30"), image=self.add_img, command=self.add_new)
-        self.add_button.grid(row=2, column=0,padx=(10,0), columnspan=1, pady=(0, 0),sticky="ew")
-        
-        self.url_entry = customtkinter.CTkEntry(self.download_frame, width = 380, placeholder_text="请粘贴视频网址")
-        self.url_entry.grid(row=2, column=1, padx=(10,10), columnspan=13, pady=(0, 0),sticky="ew")
 
+        self.shares_button = customtkinter.CTkButton(self.download_frame, corner_radius=0, height=40, border_spacing=10, text="获取网址", fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.shares_img, anchor="w", command=self.shares_button_event)
+        self.shares_button.grid(row=1, column=15, columnspan = 1)
+    
+        self.add_button =  customtkinter.CTkButton(self.download_frame, corner_radius=0, width=15,text="", fg_color="transparent",hover_color=("gray70", "gray30"), image=self.add_img, command=self.add_new)
+        self.add_button.grid(row=2, column=0,padx=(0,0), columnspan=1, pady=(0, 0))
+
+        self.url_entry = customtkinter.CTkEntry(self.download_frame, width = 380, placeholder_text="请粘贴视频网址")
+        self.url_entry.grid(row=2, column=1, padx=(10,10), columnspan=14, pady=(0, 0),sticky="ew")
+        
         self.start_down_button = customtkinter.CTkButton(self.download_frame, text="download", command=self.start_downLoad)
-        self.start_down_button.grid(row=2, column=15, columnspan=1, padx=(0,20), pady=(0,0), sticky="ew")
+        self.start_down_button.grid(row=2, column=15, columnspan=1, padx=(0,10), pady=(0,0))
         
         self.current_row = 2
-        # self.image_label = customtkinter.CTkLabel(self.download_frame, text="", image=self.bg2_img)
-        # self.image_label.grid(row=2, column=0, padx=(0,0), pady=0, columnspan = 13, sticky="ew")
        
         self.save_entry = customtkinter.CTkEntry(self.download_frame, width = 420,  placeholder_text="请选择路径")
-        self.save_entry.grid(row=11, column=0, padx=(10,0), columnspan=15, pady=0, sticky="ew")
+        self.save_entry.grid(row=11, column=0, padx=(20,0), columnspan=15, pady=0, sticky="ew")
         
-        self.buttonOpen = customtkinter.CTkButton(self.download_frame, corner_radius=0, width=15, fg_color="transparent", text = " ", hover_color=("gray70", "gray30"), image=self.open_img, command=self.select_path)
-        #self.buttonOpen = customtkinter.CTkButton(self.download_frame, image=self.open_img, command=self.open_path, text=" ")
+        self.buttonOpen = customtkinter.CTkButton(self.download_frame, corner_radius=0, width=10, fg_color="transparent", text = " ", hover_color=("gray70", "gray30"), image=self.open_img, command=self.select_path)
         self.buttonOpen.grid(row=11, column=15, columnspan=1, padx=(0,0), pady=0)
 
         self.progress_label =  customtkinter.CTkLabel(self.download_frame, corner_radius=0, height=30,  text=f"{PROGRESS_INFO}", fg_color="transparent", text_color=("gray10", "gray90"), anchor="w")
-        self.progress_label.grid(row=12, column=0, columnspan=2, padx=(0,10), pady=(0, 0) )
+        self.progress_label.grid(row=12, column=0, columnspan=16, padx=(20,0), pady=(0, 0), sticky="nsew")
         self.progressbar = customtkinter.CTkProgressBar(self.download_frame, height= 30)
-        self.progressbar.grid(row=13, column=0, columnspan=16, padx=(10,10), pady=(0, 10), sticky="nsew")
+        self.progressbar.grid(row=13, column=0, columnspan=16, padx=(20,10), pady=(0, 10), sticky="nsew")
         self.progressbar.set(0.01)
 
-        # create shares frame
+        # # create shares frame
         self.shares_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.shares_frame.grid_columnconfigure((1), weight=1)
         self.shares_frame.grid_rowconfigure((1), weight=0)
@@ -133,24 +132,32 @@ class App(customtkinter.CTk):
         self.register_frame.grid_columnconfigure((1), weight=1)
         self.register_frame.grid_rowconfigure((1), weight=0)
 
+
+        rowNum = 1
+        self.status_label =  customtkinter.CTkLabel(self.register_frame, corner_radius=0, height=30,  text=f"{STATUS_INFO}", fg_color="transparent", text_color=("gray10", "gray90"), anchor="w")
+        self.status_label.grid(row=rowNum, column=0, columnspan=5, padx=(40,0), pady=(10, 0), sticky="nsew")
+
         self.tel_label = customtkinter.CTkLabel(self.register_frame, text="TelNum:", compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.tel_label.grid(row=0, column=0, padx=(10,0), pady=(20,10))
+        self.tel_label.grid(row=rowNum+1, column=0, padx=(0,0), pady=(20,10))
         self.tel_entry = customtkinter.CTkEntry(self.register_frame, width = 50, placeholder_text="please input telNum")
-        self.tel_entry.grid(row=0, column=1, columnspan=2,sticky="nsew", padx=(5,5), pady=(20,10))
-        self.tel_info_label = customtkinter.CTkLabel(self.register_frame, text="* You must give me your tel exactly", compound="left", font=customtkinter.CTkFont(size=15))
-        self.tel_info_label.grid(row=0, column=3, padx=(0,20), pady=(20,10))
+        self.tel_entry.grid(row=rowNum+1, column=1, columnspan=4,sticky="nsew", padx=(5,5), pady=(20,10))
+        self.tel_info_label = customtkinter.CTkLabel(self.register_frame, text="* Give me your tel exactly", compound="left", font=customtkinter.CTkFont(size=15))
+        self.tel_info_label.grid(row=rowNum+1, column=5, padx=(0,20), pady=(20,10))
         self.email_label = customtkinter.CTkLabel(self.register_frame, text="Email:", compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.email_label.grid(row=1, column=0, padx=(10,0), pady=10)
+        self.email_label.grid(row=rowNum+2, column=0, padx=(0,0), pady=10)
         self.email_entry = customtkinter.CTkEntry(self.register_frame, width =50, placeholder_text="please input email")
-        self.email_entry.grid(row=1, column=1, columnspan=2,sticky="nsew", padx=(0,5), pady=10)
-        self.email_info_label = customtkinter.CTkLabel(self.register_frame, text="* You must give me your Email exactly", compound="left", font=customtkinter.CTkFont(size=15))
-        self.email_info_label.grid(row=1, column=3, padx=(0,20), pady=10)
-        self.submit_button = customtkinter.CTkButton(self.register_frame, text="submit", command=None, image=self.confirm_img)
-        self.submit_button.grid(row=2, column=1, columnspan=1, padx=(0,0), pady=20)
+        self.email_entry.grid(row=rowNum+2, column=1, columnspan=4,sticky="nsew", padx=(0,5), pady=10)
+        self.email_info_label = customtkinter.CTkLabel(self.register_frame, text="* Give me your Email exactly", compound="left", font=customtkinter.CTkFont(size=15))
+        self.email_info_label.grid(row=rowNum+2, column=5, padx=(0,20), pady=10)
+        
+        self.submit_button = customtkinter.CTkButton(self.register_frame, text="submit", command=self.register_submit_event, image=self.confirm_img)
+        self.submit_button.grid(row=rowNum+3, column=0, columnspan=1, padx=(60,0), pady=20)
+        self.VIP_button = customtkinter.CTkButton(self.register_frame, text="BeVIP", command=self.vip_button_event, image=self.more_img)
+        self.VIP_button.grid(row=rowNum+3, column=5, columnspan=1, padx=(0,20), pady=20)
 
         #create textbox
-        self.register_box = customtkinter.CTkTextbox(self.register_frame, width=280)
-        self.register_box.grid(row=3, column=0, columnspan=10, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.register_box = customtkinter.CTkTextbox(self.register_frame, width=600)
+        self.register_box.grid(row=rowNum+5, column=0, columnspan=10, padx=(20, 20), pady=(20, 0), sticky="nsew")
         self.register_box.insert("0.0", "NOTES!!!\n\n" + "You need to register, if you want to use this software  unt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 10)
 
         # create about us frame
@@ -220,6 +227,14 @@ class App(customtkinter.CTk):
         self.select_frame_by_name("aboutus")
         webbrowser.open('https://chaoxiyan1225.github.io/aboutme')
 
+    # to aboutus page view 
+    def shares_button_event(self):
+        webbrowser.open('https://chaoxiyan1225.github.io/shareurls')
+
+    # to aboutus page view 
+    def vip_button_event(self):
+        webbrowser.open('https://chaoxiyan1225.github.io/business')
+
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
@@ -237,12 +252,12 @@ class App(customtkinter.CTk):
            return
            
         url_entry = customtkinter.CTkEntry(self.download_frame, width = 380, placeholder_text="请粘贴视频网址")
-        url_entry.grid(row=self.current_row, column=1, padx=(10,10), columnspan=13, pady=(0, 0),sticky="ew")
+        url_entry.grid(row=self.current_row, column=1, padx=(10,10), columnspan=14, pady=(0, 0),sticky="ew")
         
         cha_button = customtkinter.CTkButton(self.download_frame, corner_radius=0, height=40, border_spacing=10, text=" ",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), image=self.cha_img, anchor="w", command=lambda: self.forget_row(cha_button))
                                                       
-        cha_button.grid(row=self.current_row, column=15, columnspan=1, padx=(0,20), pady=(0,0), sticky="ew")
+        cha_button.grid(row=self.current_row, column=15, columnspan=1, padx=(0,10), pady=(0,0))
     
     def forget_row(self, chaButton):  
         buttonRow = int(chaButton.grid_info()["row"])    
@@ -282,31 +297,37 @@ class App(customtkinter.CTk):
                             )
             
     def check_register_valid(self):
-        email = self.email_entry.getText()        
+        tel = self.tel_entry.get()        
+        isValid = UserUITool.IsValidTel(tel)
+
+        if isValid == False:
+            messagebox.showinfo(title="错误提示", message="输入的手机号非法")
+            self.tel_entry.configure(fg_color="red")
+            return False
+
+        email = self.email_entry.get()        
         isValid = UserUITool.IsValidEmail(email)
 
         if isValid == False:
             messagebox.showinfo(title="错误提示", message="输入的邮箱非法") 
-            return False
-
-        tel = self.tel.text()        
-        isValid = UserUITool.IsValidTel(tel)
-
-        if isValid == False:
-            messagebox.showinfo(title="错误提示", message="输入的手机号非法") 
+            self.tel_entry.configure(fg_color="white") 
+            self.email_entry.configure(fg_color="red")
             return False
 
         return True
 
 
-    def send_msg_register(self):
+    def register_submit_event(self):
 
         isValid = self.check_register_valid()
         if isValid == False:
             return
+        
+        self.email_entry.configure(fg_color="white") 
+        self.tel_entry.configure(fg_color="white")
 
         #text, ok = QInputDialog.getText(self, '用户注册提示界面', '输入邮箱')
-        isSend = self.userCtrl.clickToRegister(self.email.text(), self.tel.text())
+        isSend = self.userCtrl.clickToRegister(self.email_entry.get(), self.tel_entry.get())
         if isSend == Errors.SUCCESS:
            messagebox.showinfo(self, "成功提示", "您的注册申请{成功}注意查收邮件") 
            return True
